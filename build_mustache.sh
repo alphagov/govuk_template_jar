3cdb8b6e6e29b1a0eeac0ddec657d9af70f13163
Maria Rubio-Gonzalez <maria.rubio-gonzalez@digital.hmrc.gov.uk>
2015-03-18 15:34:25 +0000

    Change pom.xml to generate in webjars format

--- build_mustache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd module/govuk_template && bundle exec rake build:mustache && cd ../..
-mkdir -p target/generated-resources/uk/gov/template
-tar xvzf ./module/govuk_template/pkg/mustache_govuk_template-0.12.0.tgz -C target/generated-resources/uk/gov/template --strip 1
